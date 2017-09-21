60a7d69603bdf73472eb55f241c1b7335ddab6de
Mack Arnold <miarnold@madisoncollege.edu>
2017-09-20 21:25:19 -0500

    working on the tests for hibernate.

--- src/test/java/edu/matc/persistence/UserHibernateDaoTest.java
+++ src/test/java/edu/matc/persistence/UserHibernateDaoTest.java
@@ -1,20 +1,33 @@
 package edu.matc.persistence;
 
+import edu.matc.entity.User;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class UserHibernateDaoTest {
+    UserHibernateDao userDao;
+
+    @Before
+    public void setUp() throws Exception {
+        userDao = new UserHibernateDao();
+
+    }
     @Test
-    public void getAllUsers() throws Exception {
+    public void getAllUsersTest() throws Exception {
+        List<User> users = userDao.getAllUsers();
+        assertEquals("Unexpected number of users returned", 2, users.size());
     }
 
     @Test
-    public void deleteUser() throws Exception {
+    public void deleteUserTest() throws Exception {
     }
 
     @Test
-    public void updateUser() throws Exception {
+    public void updateUserTest() throws Exception {
     }
 
 }

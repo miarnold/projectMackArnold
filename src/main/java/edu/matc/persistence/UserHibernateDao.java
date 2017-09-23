807ae9ca05ea70e1287aea8cbc577af85194d075
Mack Arnold <miarnold@madisoncollege.edu>
2017-09-22 21:46:34 -0500

    working on the tsts.

--- src/main/java/edu/matc/persistence/UserHibernateDao.java
+++ src/main/java/edu/matc/persistence/UserHibernateDao.java
@@ -4,6 +4,7 @@ import edu.matc.entity.User;
 import org.apache.log4j.Logger;
 import org.hibernate.HibernateException;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,34 +33,86 @@ public class UserHibernateDao {
     }
 
 
-    public void deleteUser(User user) {
-        Session session = null;
-        try {
-            session = SessionFactoryProvider.getSessionFactory().openSession();
+    public void deleteUser(String userName){
+        Session session = SessionFactoryProvider.getSessionFactory().openSession();
+        Transaction transaction = null;
+        try{
+            transaction = session.beginTransaction();
+            User user = (User)session.get(User.class, userName);
             session.delete(user);
-        }catch (HibernateException he) {
-            log.error("Error getting all users", he);
-        } finally {
-            if (session != null) {
-                session.close();
-            }
+            transaction.commit();
+        }catch (HibernateException e) {
+            if (transaction!=null) transaction.rollback();
+            e.printStackTrace();
+        }finally {
+            session.close();
         }
     }
+
+
+    public User selectUser(String userName) {
+        User user = null;
+        Session session = SessionFactoryProvider.getSessionFactory().openSession();
+        Transaction transaction = null;
+        try{
+            transaction = session.beginTransaction();
+            user = (User)session.get(User.class, userName);
+            session.get(User.class, userName);
+            transaction.commit();
+        }catch (HibernateException e) {
+            if (transaction!=null) transaction.rollback();
+            e.printStackTrace();
+        }finally {
+            session.close();
+        }
+        return user;
+    }
+
 
 
 
     public void updateUser(User user) {
+        Transaction transaction = null;
         Session session = null;
         try {
-           session = SessionFactoryProvider.getSessionFactory().openSession();
-            session.update(user);
-        }catch (HibernateException he) {
-            log.error("Error getting all users", he);
+            session = SessionFactoryProvider.getSessionFactory().openSession();
+            transaction = session.beginTransaction();
+            session.saveOrUpdate(user);
+            transaction.commit();
+        } catch (HibernateException he){
+            if (transaction != null) {
+                try {
+                    transaction.rollback();
+                } catch (HibernateException he2) {
+                    log.error("Error rolling back update of user: " + user, he2);
+                }
+            }
         } finally {
             if (session != null) {
                 session.close();
             }
         }
     }
+
+    public User addUser(User user) {
+        Session session = SessionFactoryProvider.getSessionFactory().openSession();
+        Transaction transaction = null;
+        try{
+            transaction = session.beginTransaction();
+            user = new User( user.getUserName(), user.getUserPass());
+            session.save(user);
+            transaction.commit();
+        }catch (HibernateException e) {
+            if (transaction!=null) transaction.rollback();
+            e.printStackTrace();
+        }finally {
+            session.close();
+        }
+        return user;
+    }
+
+
+
+
 
 }
--- src/test/java/edu/matc/persistence/UserHibernateDaoTest.java
+++ src/test/java/edu/matc/persistence/UserHibernateDaoTest.java
@@ -1,6 +1,7 @@
 package edu.matc.persistence;
 
 import edu.matc.entity.User;
+import org.apache.log4j.Logger;
 import org.junit.Before;
 import org.junit.Test;
 
@@ -9,25 +10,54 @@ import java.util.List;
 import static org.junit.Assert.*;
 
 public class UserHibernateDaoTest {
+    private final Logger logger = Logger.getLogger(this.getClass());
     UserHibernateDao userDao;
+    User user;
+    int listOfUsers;
 
     @Before
     public void setUp() throws Exception {
         userDao = new UserHibernateDao();
+        listOfUsers = userDao.getAllUsers().size();
+        user = new User();
 
     }
     @Test
     public void getAllUsersTest() throws Exception {
         List<User> users = userDao.getAllUsers();
-        assertEquals("Unexpected number of users returned", 2, users.size());
-    }
-
-    @Test
-    public void deleteUserTest() throws Exception {
+        assertEquals("Unexpected number of users returned", listOfUsers, users.size());
     }
 
     @Test
     public void updateUserTest() throws Exception {
+        listOfUsers = userDao.getAllUsers().size();
+        String userName = "Aaron";
+        user = userDao.selectUser(userName);
+        user.setUserName("Sam");
+        userDao.updateUser(user);
+        assertEquals("List size has changed", listOfUsers, userDao.getAllUsers().size());
+        assertEquals("Update changes failed to save", user.toString(), userDao.selectUser(userName).toString());
     }
+
+    @Test
+    public void selectUserTest() throws Exception {
+        user = userDao.selectUser("Aaron");
+        assertNotNull(user);
+        assertEquals("wrong password returned", "briggs", user.getUserPass());
+    }
+
+    @Test
+    public void deleteUserTest() throws Exception {
+        userDao.deleteUser("Sam");
+        assertEquals("Incorrect size of results", listOfUsers - 1, userDao.getAllUsers().size());
+    }
+
+    @Test
+    public void addUserTest() {
+        user = new User( "Sam", "Soper");
+
+        userDao.addUser(user);
+        assertEquals("Incorrect size of results", listOfUsers + 1, userDao.getAllUsers().size());
+        }
 
 }

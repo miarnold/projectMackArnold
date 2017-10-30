c763fc2483afd6c22eb3fb2c0da277dbda284b0f
Mack Arnold <miarnold@madisoncollege.edu>
2017-10-30 12:02:31 -0500

    adding the add user functionalitly.

--- src/main/java/edu/matc/controller/SignUp.java
+++ src/main/java/edu/matc/controller/SignUp.java
@@ -1,5 +1,10 @@
 package edu.matc.controller;
 
+import edu.matc.entity.User;
+import edu.matc.entity.UserRole;
+import edu.matc.persistence.UserHibernateDao;
+import edu.matc.persistence.userRoleHibDao;
+
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -7,14 +12,37 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.util.logging.Logger;
 
 @WebServlet (
         urlPatterns = {"/Sign-Up"}
 )
 
 public class SignUp extends HttpServlet {
+
+    private final org.apache.log4j.Logger log = org.apache.log4j.Logger.getLogger(this.getClass());
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        UserHibernateDao userDao = new UserHibernateDao();
+        userRoleHibDao roleDao = new userRoleHibDao();
+        User addMe = new User();
+
+        addMe.setFirstName(req.getParameter("firstName"));
+        addMe.setLastName(req.getParameter("lastName"));
+        addMe.setUserName(req.getParameter("userName"));
+        addMe.setUserPass(req.getParameter("password"));
+
+
+        userDao.addUser(addMe);
+        UserRole role = new UserRole(addMe, addMe.getFirstName(),"registered-user");
+        log.error(role);
+        roleDao.addRole(role);
+
+
+
+
+
 
         RequestDispatcher dispatcher = req.getRequestDispatcher("/JSP/SignUp.jsp");
         dispatcher.forward(req, resp);

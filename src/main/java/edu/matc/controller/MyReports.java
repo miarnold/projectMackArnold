d9d8294883d9531939bcb4ed8865e90c52b59dbd
Mack Arnold <miarnold@madisoncollege.edu>
2017-11-22 17:08:20 -0600

    finished with the routes servlet hopefully.

--- src/main/java/edu/matc/controller/MyReports.java
+++ src/main/java/edu/matc/controller/MyReports.java
@@ -2,6 +2,7 @@ package edu.matc.controller;
 
 import edu.matc.entity.Routes;
 import edu.matc.persistence.CarsHibDao;
+import edu.matc.persistence.RouteDao;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -19,6 +20,7 @@ public class MyReports extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
+        RouteDao dao = new RouteDao();
         CarsHibDao carsHib = new CarsHibDao();
         req.setAttribute("cars", carsHib.getAllCars());
         String userNameOfCurrentUser = req.getRemoteUser();
@@ -30,9 +32,16 @@ public class MyReports extends HttpServlet {
         String gas = req.getParameter("gas");
         if(gas != null) {Double gasDouble = Double.parseDouble(gas);
         addRoute.setGasPrice(gasDouble);}
+        String cityMiles = req.getParameter("city");
+        if(cityMiles != null) { int cityInt = Integer.parseInt(cityMiles);
+            addRoute.setNumberOfCityMiles(cityInt); }
+        String highMiles = req.getParameter("highway");
+        if(highMiles != null) { int highInt = Integer.parseInt(highMiles);
+            addRoute.setNumberOfHighwayMiles(highInt); }
+        addRoute.setTotal(req.getParameter("total"));
+        addRoute.setWhichCar(req.getParameter("car"));
 
-
-
+        dao.addRoute(addRoute);
 
 
 

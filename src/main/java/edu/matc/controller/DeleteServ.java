53d4423006c90c09890f74eab522d341921efb54
Mack Arnold <miarnold@madisoncollege.edu>
2017-11-22 20:09:35 -0600

    got the delete user working.

---
+++ src/main/java/edu/matc/controller/DeleteServ.java
@@ -0,0 +1,43 @@
+package edu.matc.controller;
+
+import edu.matc.entity.Cars;
+import edu.matc.entity.User;
+import edu.matc.persistence.CarsHibDao;
+import edu.matc.persistence.UserHibernateDao;
+import org.apache.log4j.Logger;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@WebServlet (
+        urlPatterns = {"/DeleteServ"}
+)
+
+public class DeleteServ extends HttpServlet {
+
+    private final Logger log = Logger.getLogger(this.getClass());
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+
+
+        UserHibernateDao hibDao = new UserHibernateDao();
+        User user = new User();
+
+        int id = Integer.parseInt(req.getParameter("checkbox"));
+
+        hibDao.deleteUser(id);
+
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/JSP/userSearch.jsp");
+        dispatcher.forward(req, resp);
+    }
+
+
+}
--- src/main/webapp/JSP/userBody.jsp
+++ src/main/webapp/JSP/userBody.jsp
@@ -4,6 +4,8 @@
     <div class="container-fluid">
         <h2>Search Results: </h2>
         <!--${users}-->
+
+        <form action="DeleteServ" method="POST">
         <table id="userTable" class="display" style="width:100%">
             <th>User Name</th>
             <th>User Password</th>
@@ -14,9 +16,12 @@
                 <tr>
                     <td>${user.userName} </td>
                     <td>${user.userPass} </td>
+                    <td><input type="checkbox" value="${user.userId}" name="checkbox"/></td>
                 </tr>
             </c:forEach>
         </table>
+            <button type="submit">Delete users</button>
+        </form>
     </div>
 </div>
 </body>
